3d06e765fbd0fb3e740355a43845651eb644e393
Christopher Humphrey <cwajh.x2014@gmail.com>
2016-11-20 20:25:29 +0100

    ex-repo storage of passwerds/keys/&c

---
+++ keys.hh
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_GUARD_FOR_KEYS_HH
+#define INCLUDE_GUARD_FOR_KEYS_HH
+
+// The corresponding keys.cc is not on GitHub for painfully obvious reasons.
+
+extern byte COOKIE_SALT[32];
+
+#endif /* INCLUDE_GUARD_FOR_KEYS_HH */
