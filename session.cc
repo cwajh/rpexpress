7a1036bfc055764ebb1451e41af7cbfef518fe2c
Christopher Humphrey <cwajh.x2014@gmail.com>
2016-11-22 21:53:31 +0100

    factored out

--- session.cc
+++ session.cc
@@ -41,22 +41,11 @@ std::string salted_mac_for_string(const byte salt[32], const std::string &cookie
 
 
 std::map<std::wstring, std::wstring> session_data_for_cookies(const std::map<std::wstring, std::wstring> &cookies){
-	std::map<std::wstring, std::wstring> nothing;
-
-	std::string received_mac = base64_decode(w2s(cookies.get(SESSION_MAC_COOKIE)));
+	std::string received_mac = w2s(cookies.get(SESSION_MAC_COOKIE));
 	std::string cookie = w2s(cookies.get(SESSION_COOKIE));
 
-	if(received_mac.length() != 32) {
-		// Invalid. Should be a SHA3-256 hash.
-		return nothing;
-	}
-
-	// Chose SHA3 b/c it isn't subject to length-extension attacks (makes MAC schedule easier).
-	CryptoPP::SHA3_256 hash;
-	hash.Update(COOKIE_SALT, sizeof(COOKIE_SALT));
-	hash.Update(cookie.c_str(), cookie.length());
-	if(!hash.Verify(received_mac.c_str())) {
-		// Invalid. Cookie seems tampered with.
+	if(!mac_valid_for_string(received_mac, cookie)) {
+		std::map<std::wstring, std::wstring> nothing;
 		return nothing;
 	}
 
@@ -66,12 +55,7 @@ std::map<std::wstring, std::wstring> session_data_for_cookies(const std::map<std
 std::vector<std::pair<std::string, std::string>> headers_for_session_data(const std::map<std::wstring, std::wstring> &session_data) {
 	std::string session_cookie = query_string_for_map(session_data);
 
-	char mac[32];
-	CryptoPP::SHA3_256 hash;
-	hash.Update(COOKIE_SALT, sizeof(COOKIE_SALT));
-	hash.Update(session_cookie.c_str(), session_cookie.length());
-	hash.Final(&mac[0]);
-	std::string b64_mac = base64_encode(mac,32);
+	std::string b64_mac = salted_mac_for_string(COOKIE_SALT, session_cookie);
 
 	auto expiration_point = std::chrono::system_clock::now() + COOKIE_LIFESPAN;
 	std::time_t expiration_time = std::chrono::system_clock::to_time_t(expiration_point);
